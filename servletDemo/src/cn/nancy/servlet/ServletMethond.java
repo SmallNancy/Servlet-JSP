17299821eb95211f2ae2ffaf55f11f5a8505fd28
smallnancy <smallnancy@outlook.com>
2019-02-25 18:26:13 +0800

    request 获取信息

--- servletDemo/src/cn/nancy/servlet/ServletMethond.java
+++ servletDemo/src/cn/nancy/servlet/ServletMethond.java
@@ -17,5 +17,11 @@ public class ServletMethond extends HttpServlet {
   protected void service(HttpServletRequest req, HttpServletResponse resp) 
   		throws ServletException, IOException {
   	System.out.println("service ---");
+  	String methond = req.getMethod();      // 获取请求信息
+  	System.out.println(methond);
+  	StringBuffer sb = req.getRequestURL(); // 获取URL
+  	System.out.println("获取URL:" + sb);
+  	String uri = req.getRequestURI();      // 获取协议
+  	System.out.println("获取协议:" + uri);
   }
 }
